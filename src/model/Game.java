be88a4bba9fef30e0331259180583ff0c1b18992
camilovivas <47890754+camilovivas@users.noreply.github.com>
2019-11-13 15:06:37 -0500

    create class

---
+++ src/model/Game.java
@@ -0,0 +1,7 @@
+package model;
+
+public class Game {
+	public static int QUANTITY_HALL = 10;
+	private Hall[] famous;
+	
+}
---
+++ src/model/Hall.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Hall {
+	private String name;
+	private int score;
+
+	
+	public Hall(String name, int score) {
+		this.name = name;
+		this.score = score;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public int getScore() {
+		return score;
+	}
+
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	
+	
+	
+}
